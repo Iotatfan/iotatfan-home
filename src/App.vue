871d6efc4a082f6d866039437db2a8f0b1ca7f11
Muhammad Iqbal Imani Atfan <42773954+Iotatfan@users.noreply.github.com>
2021-09-23 20:46:37 +0700

    initialize

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <home/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+
+    Home</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+}
+
+body {
+  background: #141518 !important;
+  margin: 0%;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container-fluid flex-wrapper" ref="vantaRef">
+    <header>
+      <h1 id="fullname">MUHAMMAD IQBAL IMANI ATFAN</h1>
+      <div>
+        <p id="intro">I am <span>...</span></p>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three'
+import NET from 'vanta/dist/vanta.net.min'
+
+export default {
+    mounted() {
+        this.vantaEffect = NET({
+            el: this.$refs.vantaRef,
+            THREE: THREE
+        })
+    }
+};
+</script>
+
+<style>
+.flex-wrapper {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+header {
+    padding-top: 15%;
+    padding-inline-start: 15%;
+    padding-inline-end: 15%;
+}
+
+
+
+</style>
